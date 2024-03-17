648fe36e6f735cfb01e2b3878c56ff63e887f62c
sureshpamula <suresh_pamula@hotmail.com>
2024-03-17 12:06:51 -0400

    resolving merge conflicts

--- newmove/git_tutorial.py
+++ newmove/git_tutorial.py
@@ -3,3 +3,9 @@ def add_numbers(a,b):
     """ Function to add two numbers and increase it by 5 
     """
     return sum([a , b ,5])
+    
+'''
+1. Add doc string 
+2. Performance improvement 
+3. Security vulnerability fix
+'''
